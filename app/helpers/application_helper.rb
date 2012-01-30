c107dae0bd3526e39bc2f4124102e679e6e09daf
sampal <samik2803@gmail.com>
2012-01-30 17:51:42 -0500

    Added flash  messaging support.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,12 @@ module ApplicationHelper
     end
     elem
   end
+  
+  def render_flash(custom_flash = nil)
+    message = custom_flash || flash
+    flash_types = [:alert, :error, :success]
+    flash_type = flash_types.detect { |a| message.keys.include?(a) && message[a.to_sym].present? }
+    raw "<div id='flash_%s_div' class='%s'>%s</div>" % [flash_type.to_s, flash_type.to_s, message[flash_type]] if flash_type
+  end
 
 end
