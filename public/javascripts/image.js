b833a990c3c0d82e77f2479095aebf23fe9dbb70
Amit Kumar <toamitkumar@gmail.com>
2012-04-03 23:52:49 -0400

    The image dimensions are retained

--- public/javascripts/image.js
+++ public/javascripts/image.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function() {
+  $("#image_"+$("#image_id").val()).load(function() {
+    if($(this).width() > 950) {
+      $(this).width("950");
+      $(this).height("633");
+    }
+  });
   var edit_notes = $('#edit_notes').val() == "true";
   $('.jquery-note_1-1').jQueryNotes({
     operator: '/tags',
