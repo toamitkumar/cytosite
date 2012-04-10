9df6c5377a7d55d771242d57443acdfcad1e0516
sampal <samik2803@gmail.com>
2012-04-10 18:45:02 -0400

    Changed theme of site.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+    //find all form with class jqtransform and apply the plugin
+    $("form.jqtransform").jqTransform();
+});
