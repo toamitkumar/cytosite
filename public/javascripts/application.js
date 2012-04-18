b63d131406ccd293ad25d2ddf68a48646f6a92a2
sampal <samik2803@gmail.com>
2012-04-18 18:39:08 -0400

    Put logout and user settings in a dialog box.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,4 +4,39 @@
 $(document).ready(function() {
     //find all form with class jqtransform and apply the plugin
     $("form.jqtransform").jqTransform();
+
+    $( "#UserSettings" ).dialog({
+      autoOpen: false,
+      show: "blind",
+      hide: "blind",
+      position: getOffset($('.welcome'), 785, 20),
+      closeText: 'close',
+      draggable: false,
+      resizeable: false,
+      dialogClass: 'modal_container',
+      width: '100px',
+      closeOnEscape: false,
+      open: function(event, ui){
+        $(".ui-dialog-titlebar-close").remove();
+        $(".ui-dialog-titlebar").remove();
+        $(this).css('min-height', '0px');
+        $(this).css('height', '60px');
+      }
+    });
+
+    $('.user_det').click(function(){
+      if($('.ui-dialog').is(":visible")) {
+        $( "#UserSettings" ).dialog("close");
+      } else {
+        $( "#UserSettings" ).dialog("open");
+      }
+      return false;
+    });
+
 });
+
+function getOffset(el, offsetLeft, offsetTop) {
+    var left = el.position().left;
+    var top = el.position().top;
+    return [left + offsetLeft, top + offsetTop];
+}
