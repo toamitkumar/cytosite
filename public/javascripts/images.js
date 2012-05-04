8393aa083ebc68b095642d52814b141ccf42f6df
Amit Kumar <toamitkumar@gmail.com>
2012-05-04 00:33:39 -0400

    Fixed color and added tag image link on image listing page

--- public/javascripts/images.js
+++ public/javascripts/images.js
@@ -1,8 +1,95 @@
 jQuery(document).ready(function() {
+
+	$("a.thumb").click(function() {
+		var that = $(this);
+		console.log(this)
+		if(that.attr("data-link") != "") {
+			$("#tagImage").attr("href", that.attr("data-link"));
+		}
+	});
+
   jQuery('div.jqTransformSelectWrapper ul li a').click(function(){
     var categoryCode = jQuery('#categories').val();
     document.location.href = document.location.href + '?category_code=' + categoryCode;
   });
 
   jQuery('#image_link a').addClass("selected");
+
+  /**************************Galleriffic begins****************************************/
+  // We only want these styles applied when javascript is enabled
+  try {
+    $('div.content').css('display', 'block');
+
+    // Initially set opacity on thumbs and add
+    // additional styling for hover effect on thumbs
+    var onMouseOutOpacity = 0.67;
+    $('#thumbs ul.thumbs li, div.navigation a.pageLink').opacityrollover({
+      mouseOutOpacity:   onMouseOutOpacity,
+      mouseOverOpacity:  1.0,
+      fadeSpeed:         'fast',
+      exemptionSelector: '.selected'
+    });
+    var gallery = $('#thumbs').galleriffic({
+      delay:                     2500,
+      numThumbs:                 10,
+      preloadAhead:              10,
+      enableTopPager:            false,
+      enableBottomPager:         false,
+      imageContainerSel:         '#slideshow',
+      controlsContainerSel:      '#controls',
+      captionContainerSel:       '#caption',
+      loadingContainerSel:       '#loading',
+      renderSSControls:          true,
+      renderNavControls:         true,
+      playLinkText:              'Play Slideshow',
+      pauseLinkText:             'Pause Slideshow',
+      prevLinkText:              '&lsaquo; Previous Photo',
+      nextLinkText:              'Next Photo &rsaquo;',
+      nextPageLinkText:          'Next &rsaquo;',
+      prevPageLinkText:          '&lsaquo; Prev',
+      enableHistory:             true,
+      autoStart:                 false,
+      syncTransitions:           true,
+      defaultTransitionDuration: 500,
+      onSlideChange:             function(prevIndex, nextIndex) {
+        // 'this' refers to the gallery, which is an extension of $('#thumbs')
+        this.find('ul.thumbs').children()
+          .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+          .eq(nextIndex).fadeTo('fast', 1.0);
+        // Update the photo index display
+        this.$captionContainer.find('div.photo-index')
+          .html('Photo '+ (nextIndex+1) +' of '+ this.data.length);
+      },
+      onPageTransitionOut:       function(callback) {
+        this.fadeTo('fast', 0.0, callback);
+      },
+      onPageTransitionIn:        function() {
+        var prevPageLink = this.find('a.prev').css('visibility', 'hidden');
+        var nextPageLink = this.find('a.next').css('visibility', 'hidden');
+        
+        // Show appropriate next / prev page links
+        if (this.displayedPage > 0)
+          prevPageLink.css('visibility', 'visible');
+
+        var lastPage = this.getNumPages() - 1;
+        if (this.displayedPage < lastPage)
+          nextPageLink.css('visibility', 'visible');
+
+        this.fadeTo('fast', 1.0);
+      }
+    });
+
+    /**************** Event handlers for custom next / prev page links **********************/
+
+    gallery.find('a.prev').click(function(e) {
+      gallery.previousPage();
+      e.preventDefault();
+    });
+
+    gallery.find('a.next').click(function(e) {
+      gallery.nextPage();
+      e.preventDefault();
+    });
+    /******************Galleriffic end*****************************************************/
+  } catch(e) {}  
 });
