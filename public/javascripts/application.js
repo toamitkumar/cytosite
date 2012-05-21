0d10165c4b1c842fdca78cdd78b9a07795f9726e
Samik Pal <samikpal@Samik-Pals-MacBook-Pro.local>
2012-05-20 20:03:18 -0400

    Added twitter bootstrap dropdowns for user and admin

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(document).ready(function() {
+
+  $('.settings').dropdown();
+
+   $('.admin_settings').dropdown();
+
+});
